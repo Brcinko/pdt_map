54993d773b480a3895887aaa6b5f686814730cbf
Brcinko <lukas.klescinec@gmail.com>
2016-10-17 19:50:17 +0200

    added new files settings.py and db_connection.py

---
+++ db_connection.py
@@ -0,0 +1,15 @@
+"""
+    db_connection.py, author: Lukas Klescinec <lukas.klescinec@gmail.com>
+    FIIT Slovak University of Technology 2016
+
+    This file is part of school project on lesson Advanced Databases.
+"""
+import settings
+
+
+def open_connection():
+    pass
+
+
+def close_connection(conn):
+    pass
---
+++ settings.py
@@ -0,0 +1,8 @@
+"""
+    settings.py, author: Lukas Klescinec <lukas.klescinec@gmail.com>
+    FIIT Slovak University of Technology 2016
+
+    This file is part of school project on lesson Advanced Databases.
+"""
+
+# Database settings
