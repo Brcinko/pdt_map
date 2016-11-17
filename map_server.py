ea6263df905423dc0d59eb565fbdac36ec60e719
Brcinko <lukas.klescinec@gmail.com>
2016-11-17 19:33:26 +0100

    added render templare calls, fixed pubs_info json serializing

--- map_server.py
+++ map_server.py
@@ -5,13 +5,19 @@
     This module is part of school project on lesson Advanced Databases.
 """
 
+import pprint
 import db_connection
 import settings
 from string import replace
 
-from flask import Flask, request, jsonify, json
 
-app = Flask(__name__)
+from flask import Flask, request, jsonify, json, render_template, Response
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__, template_folder='frontend')
+cors = CORS(app, resources={r"/*": {"origins": "*",
+                                    "methods": " [GET, HEAD, POST, OPTIONS, PUT, PATCH, DELETE]",
+                                    "headers": "*"}})
 
 
 @app.route('/info', methods=['GET'])
@@ -22,23 +28,43 @@ def info():
         settings.infos[0]['postgis_ver'] = str(postgis_ver)
         return jsonify({'infos': settings.infos})
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/pubs', methods=['GET'])
+def pubs():
+    return render_template('pubs.html')
+
+
+@app.route('/pubs_building', methods=['GET'])
+def pubs_building():
+    return render_template('pubs_building.html')
+
 
 @app.route('/pubs_info', methods=['GET'])
+# @cross_origin()
 def pubs_info():
     if request.method == 'GET':
-        # query = 'SELECT name AS description, ST_AsGeoJSON(way) AS geometry FROM planet_osm_point WHERE amenity = \'pub\';'
-        query = 'SELECT row_to_json(fc) FROM ( SELECT \'FeatureCollection\' AS type, array_to_json(array_agg(f)) AS features FROM (SELECT \'Feature\' AS type , ST_AsGeoJSON(way)::json AS geometry, row_to_json((name, amenity)) AS properties FROM planet_osm_point WHERE amenity = \'pub\'   ) AS f )  AS fc;'
+        query = 'SELECT name AS description, ST_AsGeoJSON(ST_Transform(way,4326)) AS geometry FROM planet_osm_point WHERE amenity = \'pub\';'
+        # query = 'SELECT row_to_json(fc) FROM ( SELECT \'FeatureCollection\' AS type, array_to_json(array_agg(f)) AS features FROM (SELECT \'Feature\' AS type , ST_AsGeoJSON(ST_Transform(way,4326))::json AS geometry, row_to_json((name, amenity)) AS properties FROM planet_osm_point WHERE amenity = \'pub\'   ) AS f )  AS fc;'
         pubs = db_connection.execute_query(conn, query)
-        pubs = str(pubs)
-        new_pubs = replace(pubs, "f1", "description")
-        final_pubs = replace(new_pubs, "f2", "icon")
-        return json.dumps(final_pubs)
-        # return jsonify({'places': str(pubs)})
+        pubsx = list()
+        for p in pubs:
+            x = json.loads(p[1])
+            pubsx.append({"type": "Feature",
+                          "properties":
+                            {"description": p[0],
+                             "icon": "bar"},
+                          "geometry":  x })
+        return json.dumps(pubsx)
+        # return jsonify(json.dumps(pubsx))
 
 if __name__ == '__main__':
     # connect database
     conn = db_connection.open_connection()
     print "Database connected."
     # start API server
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
 
